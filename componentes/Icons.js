0b2b8313636dcb60909f2828ee20efebc0683032
lucas1004jx <lucas1004jx@gmail.com>
2019-06-20 23:35:07 +0200

    update nowignore and icons.js

--- componentes/Icons.js
+++ componentes/Icons.js
@@ -44,8 +44,8 @@ const Polygon = ({
 	> 
    <title>Polygon</title>
     <desc>Created with Sketch.</desc>
-    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-        <g id="Home" transform="translate(-23.000000, -13.000000)"  stroke-width="3">
+    <g id="Page-1" stroke="none" strokeWidth="1" fillRule="evenodd">
+        <g id="Home" transform="translate(-23.000000, -13.000000)"  strokeWidth="3">
             <g id="navbar" transform="translate(14.000000, 9.000000)">
                 <g id="gs_icon" transform="translate(34.000000, 32.500000) rotate(-330.000000) translate(-34.000000, -32.500000) translate(8.000000, 10.000000)">
                     <polygon id="Polygon" points="39 0 52 22.5 39 45 13 45 0 22.5 13 0"></polygon>
@@ -83,15 +83,15 @@ const Logo = ({
 	>
     <title>gs_icon</title>
     <desc>Created with Sketch.</desc>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Home" transform="translate(-23.000000, -13.000000)">
             <g id="navbar" transform="translate(14.000000, 9.000000)">
                 <g id="gs_icon" transform="translate(34.000000, 32.500000) rotate(-330.000000) translate(-34.000000, -32.500000) translate(8.000000, 10.000000)">
-                    <polygon id="Polygon" stroke="#17AD8D" stroke-width="3" points="39 0 52 22.5 39 45 13 45 0 22.5 13 0"></polygon>
+                    <polygon id="Polygon" stroke="#17AD8D" strokeWidth="3" points="39 0 52 22.5 39 45 13 45 0 22.5 13 0"></polygon>
                     <circle id="Oval" fill="#17AD8D" cx="26" cy="23" r="4"></circle>
                     <g id="arrow" transform="translate(25.909903, 22.909903) rotate(-45.000000) translate(-25.909903, -22.909903) translate(6.409903, 11.909903)">
                         <g transform="translate(19.500000, 11.000000) rotate(-23.000000) translate(-19.500000, -11.000000) translate(1.000000, 7.000000)">
-                            <path d="M0.701010127,4.00609665 L30.3218513,3.75" id="Line" stroke="#17AD8D" stroke-linecap="square"></path>
+                            <path d="M0.701010127,4.00609665 L30.3218513,3.75" id="Line" stroke="#17AD8D" strokeLinecap="square"></path>
                             <polygon id="Triangle" fill="#17AD8D" transform="translate(33.303990, 3.734325) rotate(-270.000000) translate(-33.303990, -3.734325) " points="33.3039897 0.976748951 36.6592529 6.49190047 29.9487266 6.49190047"></polygon>
                         </g>
                     </g>

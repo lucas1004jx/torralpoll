2b2701f319a973c71d433cbbe6c324963c26240a
lucas jin <lucas1004jx@gmail.com>
2019-07-03 00:31:21 +0200

    add user info

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,39 +1,45 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import { Icons, LoginModal } from './index';
+import { LoginContext } from '../context';
 
-const Navbar = () =>{
-  const [loginMode, setLoginMode] = useState(false);
-
+const Navbar = () => {
   const navbar_height = 56;
   const navbar_padding = 5;
 
-  const handleLogin = () => {
-    setLoginMode(true);
-  };
-  const handleClose = () => {
-    console.log('oginMode', loginMode);
-    setLoginMode(false);
-  };
+  const { loginModal, handleLogin, loginState, userProfile } = useContext(LoginContext);
+  console.log('userProfile', userProfile);
   return (
     <nav>
       <Link href='/'>
-        <a className="logo"> 
-          <Icons name='logo' style={{ width: 'auto', height: navbar_height-navbar_padding*2 }} />
+        <a className="logo">
+          <Icons name='logo' style={{ width: 'auto', height: navbar_height - navbar_padding * 2 }} />
         </a>
       </Link>
       <div>
-        
-        <span className="login" onClick={handleLogin}>
+        {!loginState && (
+          <span className="login" onClick={handleLogin}>
             SignIn
-        </span>
+          </span>
+        )}
         <Link href='/createpoll'>
           <a className="create">
-          Create poll
+            Create poll
           </a>
         </Link>
+        {loginState && (
+          <div className="profile-container">
+            <figure className="profile">
+              <img src={userProfile.imageUrl} alt="profile" />
+            </figure>
+            <div className="profile-detail">
+              <p>{userProfile.name}</p>
+              <p> {userProfile.email}</p>
+            </div>
+          </div>
+        )}
       </div>
-      {loginMode && <LoginModal onClick={handleClose} />}
+      {loginModal && <LoginModal />}
 
       <style jsx>
         {`
@@ -58,11 +64,47 @@ const Navbar = () =>{
           margin-left:25px;
           text-transform:capitalize;
           cursor:pointer;
+          display:inline-block;
         }
         .create{
           border:1px solid #fff;
           padding:8px 16px;
          
+        }
+        .profile-container{
+          position:relative;
+          display:inline-block;
+          cursor:pointer;
+        }
+        .profile{
+          width:40px;
+          height:40px;
+          border-radius:50%;
+          overflow:hidden;
+          padding:0;
+          margin:0;
+          display:inline-block;
+          margin-left:25px;
+           vertical-align:middle;
+        }
+        .profile img{
+          width:100%;
+          height:100%;
+          object-fit:cover;
+        }
+        .profile-detail{
+          position:absolute;
+          background:#fff;
+          padding:20px;
+          color:#000;
+          left:0;
+          bottom:0;
+          transform:translate(calc(-100% + 65px),100%);
+          box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+          display:none;
+        }
+        .profile-container:hover .profile-detail{
+          display:block;
         }
       `}
       </style>

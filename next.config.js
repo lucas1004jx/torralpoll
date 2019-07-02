5d1af77880e82316df952d5430ed5289aadc47be
lucas jin <lucas1004jx@gmail.com>
2019-07-03 00:29:50 +0200

    add env variable

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   target: 'serverless',
+  env: {
+    server: 'https://torralbot-back.herokuapp.com',
+    client_id: '239251067475-1ov5ieoodtk7579697b8c5r102375ojf.apps.googleusercontent.com'
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
